690a62afc5e20a19020b5363405150b6392de2be
mvaneijken <markvaneijken@gmail.com>
2024-12-19 17:38:34 +0100

    Automated signin experience

--- OmadaWeb.PS/Private/Invoke-DataFromWebDriver.ps1
+++ OmadaWeb.PS/Private/Invoke-DataFromWebDriver.ps1
@@ -15,61 +15,136 @@ function Invoke-DataFromWebDriver {
 
     Start-Sleep -Seconds 1
     $LoginMessageShown = $false
-    $CredentialsEntered = $false
+    $MfaRequestDisplayed = $false
+    $PhoneLinkActive = $false
+    if ((Get-Process | Where-Object { $_.ProcessName -eq "PhoneExperienceHost" } | Measure-Object).Count -gt 0) {
+        $PhoneLinkActive = $true
+    }
     do {
 
         if (-not $LoginMessageShown) {
             Write-Host "`r`nBrowser opened, please login! Waiting for login." -NoNewline -ForegroundColor Yellow
+            if ($Script:Credential -and ![string]::IsNullOrWhiteSpace($Script:Credential.UserName)) {
+                " Execute automated login steps for user: {0}" -f $Script:Credential.UserName | Write-Host -ForegroundColor Yellow -NoNewline
+            }
             $LoginMessageShown = $true
         }
 
         Write-Host "." -NoNewline -ForegroundColor Yellow
-        Start-Sleep -Seconds 1
-
+        Start-Sleep -Milliseconds 500
 
         if ($Script:Credential -and ![string]::IsNullOrWhiteSpace($Script:Credential.UserName)) {
+
+
 
             if ($EdgeDriver.url -like "https://login.microsoftonline.com/*") {
                 try {
                     $UserNameElementId = "i0116"
                     $PasswordElementId = "i0118"
                     $SubmitButton = "idSIButton9"
-                    $ButtonNotKeepSignedIn = "idBtn_Back"
-                    $LoginElements = $null
-                    $ButtonNotKeepSignedInElement = $null
-                    $SubmitButtonElement = $null
-                    #Wait-Debugger
-                    try {$LoginElements = $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($UserNameElementId))}catch {}
-                    try {$SubmitButtonElement = $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($SubmitButton))}catch {}
-                    try {$ButtonNotKeepSignedInElement = $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonNotKeepSignedIn))}catch {}
-Wait-Debugger
-                    if ($null -ne $ButtonNotKeepSignedInElement -and $null -ne $SubmitButtonElement -and $CredentialsEntered) {
-                        "Waiting for ButtonNotKeepSignedIn action" | Write-Verbose
-                        $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonNotKeepSignedIn)).Click()
-                    }
-                    if ($CredentialsEntered -ne $true -and $null -ne $LoginElements -and $null -ne $Script:Credential.Password) {
-                        "Waiting for LoginElements actions" | Write-Verbose
-                        $LoginElements[0].SendKeys($Script:Credential.UserName)
+                    $DisplayNameElement = "displayName"
+                    $CantAccessAccountId = "cantAccessAccount"
+                    $MfaElementId = "idRichContext_DisplaySign"
+                    $MfaRetryId1 = "idA_SAASTO_Resend"
+                    $MfaRetryId2 = "idA_SAASDS_Resend"
+                    $ButtonBackId = "idBtn_Back"
+                    $ButtonSubmitId = "idSIButton9"
+
+                    $Elements = $EdgeDriver.FindElements([OpenQA.Selenium.By]::XPath("//*[@id]"))
+                    $IdAttributes = $Elements.GetAttribute("id")
+
+                    if (     $IdAttributes -contains $UserNameElementId `
+                            -and $IdAttributes -contains $PasswordElementId `
+                            -and $IdAttributes -notcontains $ButtonBackId `
+                            -and $IdAttributes -contains $ButtonSubmitId `
+                            -and $IdAttributes -contains $CantAccessAccountId `
+                            -and $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonSubmitId)).ComputedAccessibleLabel -eq "Next" `
+                    ) {
+                        Start-Sleep -Milliseconds 500
+                        "Enter username" | Write-Verbose
+                        $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($UserNameElementId))[0].SendKeys($Script:Credential.UserName)
                         $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($SubmitButton)).Click()
-                        Start-Sleep -Seconds 1
-                        $LoginElements = $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($PasswordElementId))
-                        $LoginElements[0].SendKeys($Script:Credential.GetNetworkCredential().Password)
+                    }
+
+                    if (     $IdAttributes -notcontains $UserNameElementId `
+                            -and $IdAttributes -contains $PasswordElementId `
+                            -and $IdAttributes -contains $ButtonSubmitId `
+                            -and $IdAttributes -notcontains $CantAccessAccountId `
+                            -and $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonSubmitId)).ComputedAccessibleLabel -eq "Sign in"
+                    ) {
+                        "Enter password" | Write-Verbose
+                        $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($PasswordElementId))[0].SendKeys($Script:Credential.GetNetworkCredential().Password)
                         $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($SubmitButton)).Click()
-                        Start-Sleep -Seconds 1
-                        $CredentialsEntered = $true
-                        $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonNotKeepSignedIn)).Click()
                     }
-                    elseif ($CredentialsEntered -ne $true -and $null -ne $LoginElements -and $null -eq $Script:Credential.Password) {
-                        "Password is required for username + password login!" | Write-Warning
+
+                    if (     $IdAttributes -notcontains $UserNameElementId `
+                            -and $IdAttributes -notcontains $PasswordElementId `
+                            -and $IdAttributes -contains $SelectUserElementId
+                    ) {
+                        "Select logged-in account" | Write-Verbose
+                        if ($AccountElement.GetAttribute("data-test-id") -eq $Script:Credential.UserName) {
+                            $AccountElement.Click()
+                        }
                     }
-                    elseif($CredentialsEntered -ne $true) {
-                        "Waiting for AccountElements actions" | Write-Verbose
-                        $AccountElements = $EdgeDriver.FindElements([OpenQA.Selenium.By]::XPath("//*[@data-test-id]"))
-                        foreach ($AccountElement in $AccountElements) {
-                            if ($AccountElement.GetAttribute("data-test-id") -eq $Script:Credential.UserName) {
-                                $AccountElement.Click()
+
+                    if (     $IdAttributes -notcontains $UserNameElementId `
+                            -and $IdAttributes -notcontains $PasswordElementId `
+                            -and $IdAttributes -notcontains $SelectUserElementId `
+                            -and $IdAttributes -contains $ButtonBackId `
+                            -and $IdAttributes -contains $ButtonSubmitId `
+                            -and $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonBackId)).ComputedAccessibleLabel -eq "No" `
+                            -and $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonSubmitId)).ComputedAccessibleLabel -eq "Yes" `
+                    ) {
+                        "Decline Stay signed in? " | Write-Verbose
+                        $EdgeDriver.FindElement([OpenQA.Selenium.By]::Id($ButtonBackId)).Click()
+                    }
+
+                    if (    $IdAttributes -notcontains $UserNameElementId `
+                            -and $IdAttributes -notcontains $PasswordElementId `
+                            -and $IdAttributes -notcontains $ButtonBackId `
+                            -and ($EdgeDriver.FindElements([OpenQA.Selenium.By]::XPath("//*[@data-test-id]")) | Measure-Object).Count -gt 0) {
+
+                        "Select logged-in user " | Write-Verbose
+                        $EdgeDriver.FindElements([OpenQA.Selenium.By]::XPath("//*[@data-test-id]")) | ForEach-Object {
+                            if ($_.GetAttribute("data-test-id") -eq $Script:Credential.UserName) {
+                                $_.Click()
                             }
                         }
+                    }
+
+                    if (    $MfaRequestDisplayed -ne $true `
+                            -and $IdAttributes -notcontains $UserNameElementId `
+                            -and $IdAttributes -notcontains $PasswordElementId `
+                            -and $IdAttributes -contains $MFAElementId `
+                            -and $IdAttributes -notcontains $MfaRetryId `
+                            -and ($EdgeDriver.FindElements([OpenQA.Selenium.By]::XPath("//*[@data-test-id]")) | Measure-Object).Count -eq 0
+                    ) {
+                        $Message = "`nWaiting for your approve this sign-in request."
+                        if ($null -ne $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($MfaElementId)).Text) {
+                            if ($PhoneLinkActive) {
+                                $Message = "{0}. {1} (This value is now in your clipboard so you can paste it into your Authenticator app using PhoneLink)." -f $Message.TrimEnd("."), $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($MfaElementId)).Text
+                                $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($MfaElementId)).Text | Clip
+                            }
+                            else {
+                                $Message = "{0}. {1}" -f $Message.TrimEnd("."), $EdgeDriver.FindElements([OpenQA.Selenium.By]::Id($MfaElementId)).Text
+                            }
+                        }
+                        $Message | Write-Host -ForegroundColor Yellow
+                        $MfaRequestDisplayed = $true
+                    }
+
+                    if (    $MfaRequestDisplayed `
+                            -and $IdAttributes -notcontains $UserNameElementId `
+                            -and $IdAttributes -notcontains $PasswordElementId `
+                            -and $IdAttributes -notcontains $MFAElementId `
+                            -and (
+                            $IdAttributes -contains $MfaRetryId `
+                                -or $IdAttributes -contains $MfaRetryId2 )`
+                            -and ($EdgeDriver.FindElements([OpenQA.Selenium.By]::XPath("//*[@data-test-id]")) | Measure-Object).Count -eq 0
+                    ) {
+                        "`nMFA failed! Please retry!" | Write-Warning
+                        $MfaRequestDisplayed = $false
+                        $LoginMessageShown = $false
                     }
                 }
                 catch {}
