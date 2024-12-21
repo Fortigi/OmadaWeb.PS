52b527c8e2d3c1e40c1a43b179dc1cb1af8ba425
Mark van Eijken <mark@fortigi.nl>
2024-12-21 19:53:18 +0100

    Update

---
+++ Build/PushToPsGallery.ps1
@@ -0,0 +1,8 @@
+﻿PARAM(
+    [string]$SystemDefaultWorkingDirectory,
+    [string]$PsGalleryKey
+)
+#Get-ChildItem "$SystemDefaultWorkingDirectory" -Recurse | ForEach-Object {$_.FullName}
+#Get-ChildItem "$SystemDefaultWorkingDirectory/_OmadaWeb.PS Build" | Where-Object {$_.extension -notin (".psm1", ".psd1")} | Remove-Item -Force
+Publish-Module -Path "$SystemDefaultWorkingDirectory/_OmadaWeb.PS Build/BuildOutput/OmadaWeb.PS" -NugetAPIKey "$PsGalleryKey" -Verbose
+
