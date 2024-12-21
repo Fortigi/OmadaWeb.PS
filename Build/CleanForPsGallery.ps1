e6c1e60cf7dbc8e005422892f9d6cf645c45efd5
Mark van Eijken <mark@fortigi.nl>
2024-12-21 15:05:32 +0100

    Fix

--- Build/CleanForPsGallery.ps1
+++ Build/CleanForPsGallery.ps1
@@ -1,3 +1,3 @@
-﻿Get-ChildItem $(Pipeline.Workspace) -Recurse | ForEach-Object {$_.FullName}
+﻿Get-ChildItem "$(Pipeline.Workspace)" -Recurse | ForEach-Object {$_.FullName}
 Get-ChildItem "$(Pipeline.Workspace)/_OmadaWeb.PS Build" | Where-Object {$_.extension -notin (".psm1", ".psd1")} | Remove-Item -Force
 #Publish-Module -Path "$(Pipeline.Workspace)/_OmadaWeb.PS Build" -NugetAPIKey "$(psgallery)" -Verbose
--- Build/InstallGitHubCli.ps1
+++ Build/InstallGitHubCli.ps1
@@ -1,5 +1,5 @@
 ﻿"Folder contents" | Write-Host
-Get-ChildItem $(System.DefaultWorkingDirectory) -Recurse | ForEach-Object {$_.FullName}
+Get-ChildItem "$(System.DefaultWorkingDirectory)" -Recurse | ForEach-Object {$_.FullName}
 "Installing GitHub CLI" | Write-Host
 Invoke-WebRequest -Uri https://github.com/cli/cli/releases/download/v2.63.2/gh_2.63.2_windows_amd64.msi -OutFile gh.msi
 Start-Process msiexec.exe -Wait -ArgumentList '/i gh.msi /quiet /norestart'
