4747a95c78f3bb0ea30d1f66937bc0e7cde89bf4
mvaneijken <markvaneijken@gmail.com>
2024-12-21 20:57:31 +0100

    Add try/catch

--- Build/PushToPsGallery.ps1
+++ Build/PushToPsGallery.ps1
@@ -2,7 +2,23 @@
     [string]$SystemDefaultWorkingDirectory,
     [string]$PsGalleryKey
 )
-#Get-ChildItem "$SystemDefaultWorkingDirectory" -Recurse | ForEach-Object {$_.FullName}
+try {
+    "Folder tree for SystemDefaultWorkingDirectory:"
+    Get-ChildItem "$SystemDefaultWorkingDirectory" -Recurse | ForEach-Object { Write-Host $_.FullName }
+}
+catch {
+    Write-Host "Failed to retrieve directory tree: $_"
+}
+
+try {
+    Get-ChildItem "$SystemDefaultWorkingDirectory/_OmadaWeb.PS" -Filter *.nuspec -Recurse | Copy-Item -Destination "$SystemDefaultWorkingDirectory/_OmadaWeb.PS Build\BuildOutput\OmadaWeb.PS" -Force
+}
+catch {
+    Write-Error "Failed to copy nuspec file: $_"
+    exit 1
+}
+
 #Get-ChildItem "$SystemDefaultWorkingDirectory/_OmadaWeb.PS Build" | Where-Object {$_.extension -notin (".psm1", ".psd1")} | Remove-Item -Force
-Publish-Module -Path "$SystemDefaultWorkingDirectory/_OmadaWeb.PS Build/BuildOutput/OmadaWeb.PS" -NugetAPIKey "$PsGalleryKey" -Verbose
+#Publish-Module -Path "$SystemDefaultWorkingDirectory/_OmadaWeb.PS Build/BuildOutput/OmadaWeb.PS" -NugetAPIKey "$PsGalleryKey" -Verbose
+
 
