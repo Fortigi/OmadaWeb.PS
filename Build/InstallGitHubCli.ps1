4b8c45549101190393bd39629d8155646f09224d
Mark van Eijken <mark@fortigi.nl>
2024-12-21 14:17:22 +0100

    Add folder list

--- Build/InstallGitHubCli.ps1
+++ Build/InstallGitHubCli.ps1
@@ -1,4 +1,6 @@
-﻿"Installing GitHub CLI" | Write-Host
+﻿"Folder contents" | Write-Host
+Get-ChildItem $(System.DefaultWorkingDirectory) -Recurse | ForEach-Object {$_.FullName}
+"Installing GitHub CLI" | Write-Host
 Invoke-WebRequest -Uri https://github.com/cli/cli/releases/download/v2.63.2/gh_2.63.2_windows_amd64.msi -OutFile gh.msi
 Start-Process msiexec.exe -Wait -ArgumentList '/i gh.msi /quiet /norestart'
 Get-Item gh.msi | Remove-Item -Force
