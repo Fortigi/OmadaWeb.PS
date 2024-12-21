7ea72406d115b7ac76ea68efe701e6c98411cbc8
Mark van Eijken <mark@fortigi.nl>
2024-12-21 13:51:35 +0100

    Move build scripts from pipeline to git.

---
+++ Build/PushToGithub.ps1
@@ -0,0 +1,48 @@
+﻿try {
+    git fetch --tags
+    $latestTag = git describe --tags "$(git rev-list --tags --max-count=1)"
+    if ([string]::IsNullOrEmpty($latestTag)) {
+        Write-Error "No tags found. Exiting."
+        exit 1
+    }
+    Write-Host "Latest tag: $latestTag"
+    git checkout $latestTag
+}
+catch {
+    Write-Error "Git tag operations failed: $_"
+    exit 1
+}
+
+try {
+    $env:GH_TOKEN | gh auth login --with-token -
+    gh auth status | Write-Host
+}
+catch {
+    Write-Error "GitHub authentication failed: $_"
+    exit 1
+}
+
+try {
+    Get-Item .git* -Force | Remove-Item -Force -Recurse
+    New-Item ./GitHub -ItemType Directory | Out-Null
+    git clone https://$(githubPAT)@github.com/fortigi/OmadaWeb.PS.git ./GitHub
+    Copy-Item -Path "./Azure/*" -Destination "./GitHub/" -Recurse -Force
+}
+catch {
+    Write-Error "File operations failed: $_"
+    exit 1
+}
+
+try {
+    Set-Location ./GitHub
+    git config --global user.email "mark@fortigi.nl"
+    git config --global user.name "Mark van Eijken"
+    git add .
+    git commit -m "Release version $latestTag"
+    git push -f origin main
+    gh release create $latestTag --title "Release $latestTag" --notes "Release $latestTag"
+}
+catch {
+    Write-Error "Git or release operations failed: $_"
+    exit 1
+}
