54ce390c5de1f000e54119c87ea30ff70bfe6e1e
Mark van Eijken <mark@fortigi.nl>
2024-12-21 15:31:38 +0100

    Add param

--- Build/CleanForPsGallery.ps1
+++ Build/CleanForPsGallery.ps1
@@ -1,3 +1,7 @@
-﻿Get-ChildItem "$(Pipeline.Workspace)" -Recurse | ForEach-Object {$_.FullName}
-Get-ChildItem "$(Pipeline.Workspace)/_OmadaWeb.PS Build" | Where-Object {$_.extension -notin (".psm1", ".psd1")} | Remove-Item -Force
-#Publish-Module -Path "$(Pipeline.Workspace)/_OmadaWeb.PS Build" -NugetAPIKey "$(psgallery)" -Verbose
+﻿PARAM(
+    [string]$WorkSpaceFolder,
+    [string]$psgallery
+)
+Get-ChildItem "$WorkSpaceFolder" -Recurse | ForEach-Object {$_.FullName}
+Get-ChildItem "$WorkSpaceFolder/_OmadaWeb.PS Build" | Where-Object {$_.extension -notin (".psm1", ".psd1")} | Remove-Item -Force
+#Publish-Module -Path "$WorkSpaceFolder/_OmadaWeb.PS Build" -NugetAPIKey "$(psgallery)" -Verbose
