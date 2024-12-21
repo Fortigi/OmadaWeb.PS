4e911fc8021dc9a67810ccfacb27f6bfced35005
Mark van Eijken <mark@fortigi.nl>
2024-12-21 19:52:52 +0100

    Update

--- Build/CleanForPsGallery.ps1
+++
@@ -1,7 +0,0 @@
-﻿PARAM(
-    [string]$WorkSpaceFolder,
-    [string]$psgallery
-)
-Get-ChildItem "$WorkSpaceFolder" -Recurse | ForEach-Object {$_.FullName}
-Get-ChildItem "$WorkSpaceFolder/_OmadaWeb.PS Build" | Where-Object {$_.extension -notin (".psm1", ".psd1")} | Remove-Item -Force
-#Publish-Module -Path "$WorkSpaceFolder/_OmadaWeb.PS Build" -NugetAPIKey "$(psgallery)" -Verbose
