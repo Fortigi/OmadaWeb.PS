d2e466d028d71dec28c7b5244b07b246e338ea12
Mark van Eijken <mark@Fortigi.nl>
2025-01-08 19:00:44 +0000

    Merged PR 884: Add version check
    Add version check
    Fix duplicate included functions

---
+++ OmadaWeb.PS/Private/Get-GalleryModuleVersion.ps1
@@ -0,0 +1,23 @@
+﻿function Get-GalleryModuleVersion {
+    param (
+        [string]$ModuleName
+    )
+
+    try {
+        $ApiEndpoint = "https://www.powershellgallery.com/api/v2/FindPackagesById()?id='{0}'" -f $ModuleName
+        $Response = Invoke-RestMethod -Uri $ApiEndpoint -Method Get -Headers @{
+            "Accept" = "application/xml"
+        } -ConnectionTimeoutSeconds 1
+
+        if ($null -ne $Response) {
+            $LatestVersion = $Response | Sort-Object updated -Descending | Select-Object -First 1
+            return $LatestVersion.Properties.version
+        }
+        else {
+            return $null
+        }
+    }
+    catch {
+        return $null
+    }
+}
