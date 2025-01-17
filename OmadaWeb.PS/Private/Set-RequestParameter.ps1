eaa2391c96f21cad1c5db7b0563130e206989c5f
Azure DevOps Pipeline <devops@fortigi.nl>
2025-01-17 03:06:42 -0800

    Triggered by OmadaWeb.PS Create Artifact 20250117.1.

--- OmadaWeb.PS/Private/Set-RequestParameter.ps1
+++ OmadaWeb.PS/Private/Set-RequestParameter.ps1
@@ -1,6 +1,20 @@
 ﻿function Set-RequestParameter {
+    [CmdletBinding()]
+    PARAM(
+        [switch]$InvokeOmadaRequest
+    )
 
-    $ExcludedParameters = @("OmadaWebAuthCookieExportLocation", "InPrivate", "ForceAuthentication", "AuthenticationType", "EntraIdTenantId", "RequestType", "EdgeProfile")
+    if ($InvokeOmadaRequest) {
+        $InvokeOmadaRequestFunction = Get-Command -Name Invoke-OmadaRequest
+        $BoundParams.Keys | ForEach-Object {
+            if ($_ -notin $InvokeOmadaRequestFunction.Parameters.Keys) {
+                $ExcludedParameters += $_
+            }
+        }
+    }
+    else {
+        $ExcludedParameters = @("OmadaWebAuthCookieExportLocation", "InPrivate", "ForceAuthentication", "AuthenticationType", "EntraIdTenantId", "RequestType", "EdgeProfile")
+    }
 
     $Parameters = @{}
     $BoundParams.Keys | ForEach-Object {
