8b14e68cbfcd5901125c401ac318572a30cc8575
Azure DevOps Pipeline <devops@fortigi.nl>
2025-06-17 02:20:13 -0700

    Triggered by OmadaWeb.PS Create Artifact 20250617.2.

--- OmadaWeb.PS/Private/Set-Body.ps1
+++ OmadaWeb.PS/Private/Set-Body.ps1
@@ -1,13 +1,18 @@
 function Set-Body {
     [CmdletBinding()]
     PARAM()
-    
+
     "{0} - {1} - Add Body" -f $MyInvocation.MyCommand, $BoundParams.Method | Write-Verbose
     if ($null -eq $BoundParams.Body) {
         "{0} - Provided -Body is empty this is mandatory for a {1} command" -f $MyInvocation.MyCommand , $BoundParams.Method | Write-Error -ErrorAction "Stop"
     }
 
-    $BoundParams.Headers.Add("Content-Type", "application/json")
+    if ("Content-Type" -notin $BoundParams.Headers.Keys) {
+        $BoundParams.Headers.Add("Content-Type", "application/json")
+    }
+    else{
+        $BoundParams.Headers.'Content-Type' = "application/json"
+    }
     if ($BoundParams.Body.GetType().FullName -in @("System.Collections.Hashtable", "System.Collections.Specialized.OrderedDictionary", "System.Management.Automation.PSCustomObject")) {
         "{0} - Provided -Body data type is {1}, converting it to json" -f $MyInvocation.MyCommand, $BoundParams.Body.GetType().FullName | Write-Verbose
         $BoundParams.Body = $BoundParams.Body | ConvertTo-Json
